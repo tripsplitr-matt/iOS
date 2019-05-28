2cd05c61963f8dabf044e8b7679b8f9f97d76878
Ryan Murphy <thomasryanmurphy@gmail.com>
2019-05-28 12:01:55 -0400

    New Xcode file

---
+++ Trip Splitr/Trip Splitr/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Trip Splitr
+//
+//  Created by Ryan Murphy on 5/28/19.
+//  Copyright © 2019 Ryan Murphy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
