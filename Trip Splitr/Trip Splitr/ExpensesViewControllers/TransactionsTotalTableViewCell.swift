75cff16c60a7ff05117ab09a575d915aa15fbb37
Ryan Murphy <thomasryanmurphy@gmail.com>
2019-05-29 15:54:29 -0400

    rectified conversion issue ...

---
+++ Trip Splitr/Trip Splitr/ExpensesViewControllers/TransactionsTotalTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TransactionsTotalTableViewCell.swift
+//  Trip Splitr
+//
+//  Created by Jonathan Ferrer on 5/29/19.
+//  Copyright © 2019 Ryan Murphy. All rights reserved.
+//
+
+import UIKit
+
+class TransactionsTotalTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
