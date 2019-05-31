d4063a7953d8851350b6775f0aa22ebb31f6b8c5
jayfenam <jonathanferrer95@gmail.com>
2019-05-31 01:58:15 -0400

    Added single selection for paidBy collectionView....changes the label to show isenabled

--- Trip Splitr/Trip Splitr/CollectionViewCells/PaidByCollectionViewController.swift
+++ Trip Splitr/Trip Splitr/CollectionViewCells/PaidByCollectionViewController.swift
@@ -15,13 +15,11 @@ class PaidByCollectionViewController: UICollectionViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
 
-        // Register cell classes
         self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+        collectionView.allowsMultipleSelection = false
 
-        // Do any additional setup after loading the view.
+
     }
 
     /*
@@ -81,8 +79,14 @@ class PaidByCollectionViewController: UICollectionViewController {
 
         let cell = collectionView.cellForItem(at: indexPath) as! PaidByCollectionViewCell
         cell.paidLabel.isHidden = !cell.paidLabel.isHidden
+        }
 
-        
+
+    override func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
+
+
+        let cell = collectionView.cellForItem(at: indexPath) as! PaidByCollectionViewCell
+        cell.paidLabel.isHidden = !cell.paidLabel.isHidden
     }
 
     // MARK: UICollectionViewDelegate
